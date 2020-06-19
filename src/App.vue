651b6f014956fe847d41ed0ee7971e95f1133067
German <gldeperosanz@gmail.com>
2020-06-18 21:44:06 -0300

    cambios en navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav" v-if="$store.state.isLogin">
+   <!--  <div id="nav" v-if="$store.state.isLogin">
       <router-link to="/gastos">Gastos</router-link> |
       <router-link to="/empleados">Empleados</router-link> |
       <router-link to="/viajes">Viajes</router-link> |
       <router-link to="/login">Log out</router-link>
-    </div>
+    </div> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" v-if="$store.state.isLogin">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
 
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><router-link to="/gastos">Gastos</router-link></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><router-link to="/empleados">Empleados</router-link></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><router-link to="/viajes">Viajes</router-link></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <a class="nav-link" href="#"><router-link to="/login">Log out</router-link></a>
+    </form>
+  </div>
+</nav>
     <router-view/>
   </div>
 </template>
