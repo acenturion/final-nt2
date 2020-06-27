9d5364c522219e4459961f198d2cf6366aba15ba
Richard Valdez <rvaldez.valdez@gmail.com>
2020-06-27 05:25:32 -0300

    abm para rendicion de gastos

--- src/components/gastos.vue
+++ src/components/gastos.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container text-left w-75 mt-3">
-        <!--        formulario tareasl -->
+        <!--        formulario gastos -->
         <div>
-            <h2>Agregar gastos</h2>
+            <h2>Agregar Gastos</h2>
             <hr/>
             <vue-form :state="formState" @submit.prevent="sendForm()">
 
                 <validate class="form-group" tag="div">
-                    <label for="idViaje">idViaje</label>
+                    <label for="idViaje">Identificador del Viaje</label>
                     <input
                             type="number"
                             class="form-control"
@@ -20,70 +20,144 @@
                     >
                 </validate>
                 <field-messages name="idViaje" show="$dirty">
-                    <div slot="required" class="alert alert-danger my-1">La idViaje es requerida</div>
-                    <div slot="minlength" class="alert alert-danger my-1">La idViaje debe tener mas de {{minLength}}
-                        caracteres
-                    </div>
-                    <div slot="maxlength" class="alert alert-danger my-1">La idViaje debe tener menos de {{maxLength}}
-                        caracteres
-                    </div>
+                    <div slot="required" class="alert alert-danger my-1">El id del viaje es requerida</div>
+                    <div slot="minlength" class="alert alert-danger my-1">El id del viaje debe tener mas de {{minLength}} caracteres</div>
+                    <!-- <div slot="maxlength" class="alert alert-danger my-1">La idViaje debe tener menos de {{maxLength}} caracteres</div> -->
                 </field-messages>
 
                 <validate class="form-group" tag="div">
-                    <label for="name">idTipoGasto</label>
+                    <label for="idTipoGasto">Tipo de Gasto</label>
                     <input
                             type="number"
                             class="form-control"
-                            id="name"
-                            name="name"
-                            v-model.trim="formData.name"
+                            id="idTipoGasto"
+                            name="idTipoGasto"
+                            v-model.trim="formData.idTipoGasto"
+                            :minlength="minLength"
+                            :maxlength="maxLength"
                             required
                     >
                 </validate>
-                <field-messages name="name" show="$dirty">
-                    <div slot="required" class="alert alert-danger my-1">El nombre es requerido</div>
+                <field-messages name="idTipoGasto" show="$dirty">
+                    <div slot="required" class="alert alert-danger my-1">La idTipoGasto es requerida</div>
+                    <div slot="minlength" class="alert alert-danger my-1">La idTipoGasto debe tener mas de {{minLength}} caracteres</div>
+                    <!-- <div slot="maxlength" class="alert alert-danger my-1">La idTipoGasto debe tener menos de {{maxLength}} caracteres</div> -->
                 </field-messages>
 
-
                 <validate class="form-group" tag="div">
-                    <label for="importe">importe</label>
+                    <label for="importe">Importe</label>
                     <input
                             type="number"
                             class="form-control"
                             id="importe"
                             name="importe"
                             v-model.trim="formData.importe"
+                            :minlength="minLength"
+                            :maxlength="maxLength"
                             required
                     >
                 </validate>
                 <field-messages name="importe" show="$dirty">
-                    <div slot="required" class="alert alert-danger my-1">el importe es requerido</div>
-                    <div slot="importe" class="alert alert-danger my-1">Importe no válido</div>
+                    <div slot="required" class="alert alert-danger my-1">La importe es requerida</div>
+                    <div slot="minlength" class="alert alert-danger my-1">La importe debe tener mas de {{minLength}} caracteres</div>
+                    <!-- <div slot="maxlength" class="alert alert-danger my-1">La importe debe tener menos de {{maxLength}} caracteres</div> -->
                 </field-messages>
 
-                <button type="submit" class="btn btn-primary" :disabled="formState.$invalid">Submit</button>
+                <validate class="form-group" tag="div">
+                    <label for="notas">Notas</label>
+                    <input
+                            type="text"
+                            class="form-control"
+                            id="notas"
+                            name="notas"
+                            v-model.trim="formData.notas"
+                            :minlength="minLength"
+                            :maxlength="maxLength"
+                            required
+                    >
+                </validate>
+                <field-messages name="notas" show="$dirty">
+                    <div slot="required" class="alert alert-danger my-1">La notas es requerida</div>
+                    <div slot="minlength" class="alert alert-danger my-1">La notas debe tener mas de {{minLength}} caracteres</div>
+                    <div slot="maxlength" class="alert alert-danger my-1">La notas debe tener menos de {{maxLength}} caracteres</div>
+                </field-messages>
 
+                <validate class="form-group" tag="div">
+                    <label for="idFormaPago">Medio de Pago</label>
+                    <input
+                            type="number"
+                            class="form-control"
+                            id="idFormaPago"
+                            name="idFormaPago"
+                            v-model.trim="formData.idFormaPago"
+                            :minlength="minLength"
+                            :maxlength="maxLength"
+                            required
+                    >
+                </validate>
+                <field-messages name="idFormaPago" show="$dirty">
+                    <div slot="required" class="alert alert-danger my-1">El medio de pago es requerida</div>
+<!--                     <div slot="minlength" class="alert alert-danger my-1">La idFormaPago debe tener mas de {{minLength}} caracteres</div>
+                    <div slot="maxlength" class="alert alert-danger my-1">La idFormaPago debe tener menos de {{maxLength}} caracteres</div> -->
+                </field-messages>
+
+                <validate class="form-group" tag="div">
+                    <label for="fecha">Fecha del Ticket</label>
+                    <input
+                            type="date"
+                            class="form-control"
+                            id="fecha"
+                            name="fecha"
+                            v-model.trim="formData.fecha"
+                            :minlength="minLength"
+                            :maxlength="maxLength"
+                            required
+                    >
+                </validate>
+                <field-messages name="fecha" show="$dirty">
+                    <div slot="required" class="alert alert-danger my-1">La fecha es requerida</div>
+<!--                     <div slot="minlength" class="alert alert-danger my-1">La fecha debe tener mas de {{minLength}} caracteres</div>
+                    <div slot="maxlength" class="alert alert-danger my-1">La fecha debe tener menos de {{maxLength}} caracteres</div> -->
+                </field-messages>
+
+                <validate class="form-group" tag="div">
+                    <label for="foto">Foto</label>
+                    <input
+                            type="file"
+                            class="form-control"
+                            id="foto"
+                            name="foto"
+                    >
+                </validate>
+                <field-messages name="foto" show="$dirty">
+                    <div slot="required" class="alert alert-danger my-1">La foto es requerida</div>
+                </field-messages>
+
+                <button type="submit" class="btn btn-primary" :disabled="formState.$invalid">Enviar</button>
+                <button type="button" v-on:click="enviarGastoEditado()" class="btn btn-warning mx-4" :disabled="formState.$invalid">Editar</button>
             </vue-form>
-
-
         </div>
+        <!--    Alert!-->
+        <div class="alert alert-primary my-5" v-if="message" role="alert">{{message}}</div>
 
-        <!--    Display tareas -->
+        <!--    Display gastos -->
         <div class="mt-5 mb-5">
             <h2>Gastos</h2>
-            <hr/>
+            <hr />
             <table class="table">
                 <thead class="thead-dark">
                 <tr>
-                    <th scope="col">idDetalle</th>
-                    <th scope="col">idViaje</th>
-                    <th scope="col">idTipoGasto</th>
-                    <th scope="col">importe</th>
-                    <th scope="col">notas</th>
-                    <th scope="col">idFormaPago</th>
-                    <th scope="col">fecha</th>
-                    <th scope="col">aprobado</th>
-                    <th scope="col">foto</th>
+                    <th scope="col">Gasto</th>
+                    <th scope="col">Viaje</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Importe</th>
+                    <th scope="col">Notas</th>
+                    <th scope="col">Medio</th>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Aprobado</th>
+                    <th scope="col">Foto</th>
+                    <th scope="col">Editar</th>
+                    <th scope="col">Borrar</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -96,7 +170,27 @@
                     <td>{{gasto.idFormaPago}}</td>
                     <td>{{gasto.fecha | formatDate }}</td>
                     <td>{{gasto.aprobado}}</td>
-                    <td>#link</td>
+                    <div v-if="gasto.foto != null">
+                        <!-- <td><img :src="gasto.foto.data" :alt="gasto.notas"></td> -->
+                        <td>:)</td>
+                    </div>
+                    <div v-else>
+                        <td>x</td>
+                    </div>
+                    <td>
+                        <button
+                            class="btn btn-warning"
+                            v-on:click="editarGasto(gasto)"
+                        >Editar
+                        </button>
+                    </td>
+                    <td>
+                        <button
+                            class="btn btn-danger"
+                            v-on:click="eliminarGasto(gasto.idDetalle)"
+                        >Borrar
+                        </button>
+                    </td>
                 </tr>
                 </tbody>
             </table>
@@ -105,48 +199,84 @@
 </template>
 
 <script lang="js">
-    //import gastoService from '../services/gasto.service.js'
-    import Axios from 'axios'
+    import GastoService from '../services/gasto.service.js'
     export default {
-        name: 'src-components-tareas',
+        name: 'src-components-gasto',
         props: [],
-        mounted(){
-            this.getGastos()
-        },
-        beforeCreate() {
-            //this.$store.commit('setLogin', true);
-        },
+        mounted() {},
         data() {
             return {
-                gastos:[],
+                gastos: this.cargarGastos(),
                 formState: {},
                 formData: this.getInitialData(),
-                minLength: 10,
-                maxLength: 50
+                minLength: 1,
+                maxLength: 50,
+                message: null,
+                error: {
+                    status: false,
+                    msg: ''
+                }
             }
         },
         methods: {
             getInitialData() {
                 return {
-                    description: '',
-                    name: '',
-                    email: '',
+                    //idDetalle: '',
+                    idViaje: '14',
+                    idTipoGasto: '10',
+                    importe: '123.45',
+                    notas: 'taxi',
+                    idFormaPago: '3',
+                    fecha: '2020-01-05',
+                    aprobado: ''
+                    //foto: ''
                 }
             },
             sendForm() {
-                console.log(this.formData)
-                this.$store.dispatch('addTarea', this.formData)
-                this.getInitialData()
+              GastoService.addGasto(this.formData).then(
+                res => {
+                  this.message = `Se agrego el gasto [${res.data.idDetalle}] ${this.formData.idViaje}`
+                  this.cargarGastos();
+                  this.getInitialData()
+                }
+              ).catch(err => {
+                this.message = `Ocurrio un error al agregar un gasto` + err
+              })
             },
-            getGastos(){
-                Axios.get('http://localhost:8080/api/detallegasto')
-                .then(res => {
-                        this.gastos = res.data
-                        console.log(res.data)
-                })
-                .catch(err => {
-                    console.log("Error from service,", err)
-                })
+            editarGasto(gasto) {
+              this.formData = gasto
+            },
+            eliminarGasto(id) {
+              GastoService.delGasto(id).then(
+                res => {
+                  this.message = `[${res.data.idDetalle}] Se elimino el gasto`
+                  this.cargarGastos();
+                }
+              ).catch(err => {
+                //console.log(err)
+                //todo cambiar en el back la respuesta
+                this.message = `Ocurrio un error al eliminar el gasto` + err
+              });
+            },
+            enviarGastoEditado() {
+              GastoService.editGasto(this.formData).then(
+                res => {
+                  this.message = `Se edito el gasto [${res.data.idDetalle}]`
+                  this.cargarGastos()
+                  this.formData = this.getInitialData();
+                }
+              ).catch(err => {
+                this.message = `Ocurrio un error al editar un gasto` + err
+              })
+            },
+            cargarGastos(){
+              GastoService.getGastos().then(
+                res => {
+                    this.gastos = res.data;
+                }
+              ).catch(err => {
+                this.message = `Ocurrio un error al cargar un gastos` + err
+              })
             }
         },
         computed: {
--- src/services/gasto.service.js
+++ src/services/gasto.service.js
@@ -1,51 +1,28 @@
 import Axios from 'axios'
 
 function getGastos() {
-    Axios.get('http://localhost:8080/api/detallegasto')
-        .then(res => {
-                console.log("Gatos from service:", res);
-                return res;
-            },
-            err => {
-                console.log("Error from service,", err);
-            }
-        )
+    return Axios.get('http://localhost:8080/api/detallegasto')
 }
 
 function delGasto(id) {
-    console.log('Desde servicio Gatos', id);
-    Axios.delete('http://localhost:8080/api/detallegasto', {
+    return Axios.delete('http://localhost:8080/api/detallegasto', {
         data: {
-            idGasto: id
+            idDetalle: id
         }
     })
-        .then(
-            res => {
-                console.log("Se elimino el gasto " + id);
-                console.log(res);
-            }, err => {
-                console.log(err);
-                this.error.msg = 'no se pudo eliminar el gasto'
-                this.error.status = true;
-            })
 }
 
 function addGasto(gasto) {
- Axios.post('http://localhost:8080/api/detallegasto',gasto)
-     .then(
-         res =>{
-             console.log("Agregado gasto from service:", res);
-             console.log(res)
-         },
-         err =>{
-             console.log("Error al agregar gasto from service:", err);
-             console.log(err)
-         }
-     )
+    return Axios.post('http://localhost:8080/api/detallegasto', gasto)
+}
+
+function editGasto(gasto) {
+    return Axios.put('http://localhost:8080/api/detallegasto', gasto);
 }
 
 export default {
     getGastos,
     delGasto,
-    addGasto
+    addGasto,
+    editGasto
 }
